5d5687ec2a504bb0fb64b04954d50dac9565e309
Nick <cascella3509@gmail.com>
2021-09-18 17:01:30 -0400

    Continuing basic formatting on home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,52 @@
 import "../components/HomePage.css";
 
 const HomePage = () => {
+  const tweetObj = {
+    tweet: (e) => {
+      e.preventDefault();
+    },
+  };
+
   return (
     <div id="HomePage">
       <div id="HomePageTweetsLS">
-        <div>Home</div>
+        <div id="HomePageHomeHeader">
+          <div id="HomePageHomeHeaderText">Home</div>
+        </div>
+        <div id="HomePageTweetingDiv">
+          <div className="HomePageTweetProfilePicture"></div>
+          <div>
+            <form
+              onSubmit={(e) => {
+                tweetObj.tweet(e);
+              }}
+              className="HomePageTweetDivInputContainer"
+            >
+              <textarea
+                className="HomePageTweetInput"
+                placeholder="What is happening?"
+              ></textarea>
+              <button type="submit" className="HomePageTweetButton">
+                Tweet
+              </button>
+            </form>
+          </div>
+        </div>
+        <div className="HomePageTweetsDisplay">
+          <div className="IndividualTweetFormatMain">
+            <div className="HomePageTweetProfilePicture"></div>
+            <div className="IndividualTweetFormatRS">
+              <div className="IndividualTweetFormatUserInfo">
+                <div className="IndvidualTweetFormatUserText">
+                  <b>UserName</b>
+                </div>
+                <div className="IndvidualTweetFormatUserText">@sososo</div>
+                <div className="IndvidualTweetFormatUserText">Date</div>
+              </div>
+              <div className="IndividualTweetFormatTweet">Hello</div>
+            </div>
+          </div>
+        </div>
       </div>
       <div id="HomePageWhatIsHappeningRS">
         <div>What is happening</div>
